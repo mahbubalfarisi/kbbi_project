e904abf171a7394c9ce5a2c2fa8c7bfe74674c29
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-15 19:10:42 +0000

    Upload new file

---
+++ member2.php
@@ -0,0 +1,71 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		
+		<!-- Gambar tab browser -->
+		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
+		
+		<!-- Judul tab browser -->
+		<title>Member - KBBI</title>
+		
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" >
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+         
+        <!-- Style CSS -->
+        <link href="css/style.css" rel="stylesheet">
+		
+		<!-- Font -->
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
+    </head>
+		<?php
+			require('./header.php');
+		?>
+	
+    <body>
+        <div class="member2-container">
+			<div class="gambar-tepi-flip">
+				<img src="img/icon/ikonyellow.png" alt="yellowikon"/>
+			</div>
+			<div class="konten-member2judul">
+				MEMBER KAMI
+				<div class="logomember">
+					<img src="img/logo/seraci.png" alt="member1" height="100%" width="50%"/>
+				</div>
+				<div class="konten-member2juduljudul">
+					</br>
+					</br>
+					</br>
+					</br>
+					</br>
+					</br>
+					</br>
+					</br>
+						 SERACI BATIK BETAWI
+					 <div class="konten-member2isi">
+						</br>
+						</br>
+						</br>
+						</br>
+						Kp. Kebon Kelapa RT 002/RW 05</br>
+						Ds. Segarjaya, Kec. Tarumajaya,  </br>
+						Marunda, Bekasi</br>
+						Phone: +62 21 3231101</br>
+						<a href="#" style="color:#000">www.seracibatikbetawi.wordpress.com</a></br>
+						<a class="buttonnext" href="member"><span>Next </span></a>
+					</div>
+				</div>
+			</div>
+			<div class="foto-kanan-member2">
+				<img src="img/web/member2.png" alt="about" height="100%" width="100%"/>
+			</div>
+        </div>
+		
+		<!-- JS -->
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery-3.1.1.js"></script>
+		<script src="js/carousel.js"></script>
+    </body>
+</html>
