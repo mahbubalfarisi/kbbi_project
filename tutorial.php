668a226b61639233074f3b42dda1d1fc6cfc96a4
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-15 19:33:41 +0000

    Upload new file

---
+++ tutorial.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		
+		<!-- Gambar tab browser -->
+		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
+		
+		<!-- Judul tab browser -->
+		<title>Motif Batik Betawi - KBBI</title>
+		
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" >
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+         
+        <!-- Style CSS -->
+        <link href="css/style.css" rel="stylesheet">
+		
+		<!-- Font -->
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
+    </head>
+		<?php
+			require('./header.php');
+		?>
+    <body>
+        <div class="tutor-container">
+			<div class="gambar-tepi">
+				<img src="img/icon/ikonpink.png" alt="pinkikon"/>
+			</div>
+			<div class="konten-tutorjudul">
+				<h1> KIAT BERKAIN CANTIK </h1>
+				<div class="konten-tutorisi">
+					</br>
+					</br>
+					</br>
+					</br>
+						Berkebaya yang serasi dapat tercipta dari pemilihan model kain yang dapat disesuaikan dengan kebaya dan batik yang dikenakakan. Batik Betawi mempunyai berberapa kiat agar kain terlihat cantik dan menarik.
+				</div>
+			</div>
+			<div class="foto-kanan-tutor">
+				<img src="img/web/tutor.png" alt="about" height="100%" width="100%"/>
+			</div>
+        </div>
+		
+		<!-- JS -->
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery-3.1.1.js"></script>
+		<script src="js/carousel.js"></script>
+    </body>
+</html>
