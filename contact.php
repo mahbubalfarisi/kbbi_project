61aaecb5a919b2f8b99e13d6c22554c83aa0126c
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-15 19:17:37 +0000

    Upload new file

---
+++ contact.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		
+		<!-- Gambar tab browser -->
+		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
+		
+		<!-- Judul tab browser -->
+		<title>Contact - KBBI</title>
+		
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" >
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+         
+        <!-- Style CSS -->
+        <link href="css/style.css" rel="stylesheet">
+		
+		<!-- Font -->
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
+    </head>
+		<?php
+			require('./header.php');
+		?>
+	
+    <body>
+        <div class="contact-container">
+			<div class="gambar-tepi">
+				<img src="img/icon/ikonyellow.png" alt="yellowikon"/>
+			</div>
+			<div class="konten-contactjudul">
+				KONTAK KAMI
+				<div class="konten-contactisi">
+					</br>
+					</br>
+					</br>
+						WORKSHOP : </br>
+						Kp. Budaya Betawi Setu Babakan Jl. Kramat bambu No. 77 RT. 012/RW. 008 Kelurahan Srengseng Sawah Kecamatan Jagakarsa </br></br> PHONE :</br>0811 903 870,  0852 1740 5011, 0895 3390 25897
+				</div>
+			</div>
+			<div class="foto-kanan-contact">
+				<img src="img/web/contact.png" alt="about" height="100%" width="100%"/>
+			</div>
+        </div>
+		
+		<!-- JS -->
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery-3.1.1.js"></script>
+		<script src="js/carousel.js"></script>
+    </body>
+</html>
