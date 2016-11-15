28b7a246d326225d24a75bf58b941523dd0e72ab
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-15 16:15:24 +0000

    Upload new file

---
+++ header.php
@@ -0,0 +1,102 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<!-- Menyesuaikan layar -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<!-- Gambar tab browser -->
+		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
+		
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" >
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+         
+        <!-- Style CSS -->
+        <link href="css/style.css" rel="stylesheet">
+		
+		<!-- Font -->
+    </head>
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
+    <body>
+		<div class="header-container navbar-fixed-top">
+			<nav class="navbar">
+				<div class="row">
+					<div class="col-md-2">
+						<div class="header-logo">
+							<a href="home">
+								<img src="img/logo/logoteks.png" class="img-responsive" alt="logo-kbbi" />
+							</a>
+						</div>
+					</div>
+					<div class="col-md-7">
+						<div class="menu-header">
+							<div class="collapse navbar-collapse" id="myNavbar">
+								<ul class="nav navbar-nav">
+									<li><a href="home">BERANDA</a></li>
+									<li>
+										<div class="dropdown">
+											<button class="dropbtn">TENTANG KAMI</button>
+											<div class="dropdown-content">
+												<a href="about">Tentang KBBI</a>
+												<a href="sejbatikbetawi">Sejarah Batik Betawi</a>
+												<a href="motifbatikbetawi">Motif Batik Betawi</a>
+												<a href="serasibatikbetawi">Serasi Batik Betawi</a>
+												<a href="contact">Kontak Kami</a>
+											</div>
+										</div>
+									</li>
+									<li>
+										<div class="dropdown">
+											<button class="dropbtn">GALERI</button>
+											<div class="dropdown-content2">
+												<a href="batikmonas">Motif Batik Monas</a>
+												<a href="batikonon">Motif Batik Ondel-Ondel</a>
+												<a href="batiktumpal">Motif Tumpal Betawi</a>
+												<a href="gunungtumpal">Motif Gunung Tumpal</a>
+												<a href="ononpucuk">Motif Ondel-Ondel Pucuk Rebung</a>
+											</div>
+										</div>
+									</li>
+									<li><a href="tutorial">TUTORIAL</a></li>
+									<li><a href="event">EVENT</a></li>
+									<li><a href="member">MEMBER KAMI</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-1">
+						<div class="social-header">
+							<a href="#" class="btn-social">
+								<img src="img/icon/twitlogo.png" class="img-rounded" alt="Twitter Button" />
+							</a>
+							<a href="#" class="btn-social">
+								<img src="img/icon/fblogo.png" class="img-rounded" alt="Facebook Button"/>
+							</a>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="search-header">
+							<form method="get" action="search">
+								<div class="input-group stylish-input-group">
+									<input type="text" class="form-control"  placeholder="Search" >
+										<span class="input-group-addon">
+											<button type="submit">
+												<span class="glyphicon glyphicon-search"></span>
+											</button>  
+										</span>
+									</input>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+		<!-- JS -->
+		<script src="js/search.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery-3.1.1.js"></script>
+    </body>
+</html>
