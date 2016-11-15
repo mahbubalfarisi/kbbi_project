b7734f7866645e1d90251c20e4032adaa7eeaf7b
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-15 16:14:17 +0000

    Upload new file

---
+++ event.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		
+		<!-- Gambar tab browser -->
+		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
+		
+		<!-- Judul tab browser -->
+		<title>Event - KBBI</title>
+		
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" >
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+         
+        <!-- Style CSS -->
+        <link href="css/style.css" rel="stylesheet">
+		
+		<!-- Font -->
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
+    </head>
+		<?php
+			require('./header.php');
+		?>
+    <body>
+        <div class="event-container">
+			<div class="foto-kanan-event">
+				<img src="img/web/event.png" alt="about" height="100%" width="100%"/>
+			</div>
+        </div>
+		
+		<!-- JS -->
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery-3.1.1.js"></script>
+		<script src="js/carousel.js"></script>
+    </body>
+</html>
