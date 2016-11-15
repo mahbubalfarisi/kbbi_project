281357414859ffdd0454b6c32dbea69d5c295909
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-15 16:36:27 +0000

    Upload new file

---
+++ sejbatikbetawi.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		
+		<!-- Gambar tab browser -->
+		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
+		
+		<!-- Judul tab browser -->
+		<title>Sejarah Batik Betawi - KBBI</title>
+		
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" >
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+         
+        <!-- Style CSS -->
+        <link href="css/style.css" rel="stylesheet">
+		
+		<!-- Font -->
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
+    </head>
+		<?php
+			require('./header.php');
+		?>
+	
+    <body>
+        <div class="sejarahbb-container">
+			<div class="gambar-tepi">
+				<img src="img/icon/ikonyellow.png" alt="yellowikon"/>
+			</div>
+			<div class="konten-sejarah">
+				<h1> SEJARAH </h1>
+				<h1> BATIK BETAWI </h1>
+				<div class="konten-sejarahb">
+					</br>
+					</br>
+					</br>
+					</br>
+					</br>
+						Keberadaan Batik Betawi terkait erat dengan sejarah perkembangan batik Jakarta, yang telah dimulai sejak masa kolonial. Pada tahun 1986, telah berdiri bengkel pembatikan yang didirikan oleh nyonya Winkel Hagen, ia membuat batik di bengkelnya sendiri dan memasarkan batik khas Jawa Tengah yang dibuatnya di Semarang. Batik-Batik yang dibuat di bengkel Jakarta kebanyakan dikonsumsi oleh masyarakat Belanda di Batavia. Beberapa tahun kemudian muncul pembatik dan pengusaha Tionghoa yang membuka bengkelnya di wilayah Tanah Abang. Pembatik-pembatik  Tionghoa ini memproduksi batik bergaya Cina seperti yang dilakukan oleh pembatik Tionghoa di Pekalongan maupun pembatik dari Lasem. Memasuki abad ke-18, mereka di Tanah Abang memproduksi batik yang berorientasi pasar dengan menyediakan batik Jawa pesisiran dan batik bergaya Cina. Di antaranya adalah ‘Boketan Cina’ yang sangat digemari oleh penduduk Batavia. Motif Batik Betawi memiliki pengaruh lingkungan, falsafah, dan adat istiadat orang Betawi serta dipengaruhi oleh budaya Cina Warna Batik Betawi mempunyai warna-warna yang mencolok seperti merah, hijau, oranye, dan kuning. 
+				</div>
+			</div>
+			<div class="foto-kanan-sejarah">
+				<img src="img/web/fotosejarahbetawi.png" alt="about" height="100%" width="100%"/>
+			</div>
+        </div>
+		
+		<!-- JS -->
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery-3.1.1.js"></script>
+		<script src="js/carousel.js"></script>
+    </body>
+</html>
