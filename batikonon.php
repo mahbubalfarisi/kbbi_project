3144ceaf50124ede5b0a259377f0d9074efbd4af
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-15 16:13:24 +0000

    Upload new file

---
+++ batikonon.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		
+		<!-- Gambar tab browser -->
+		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
+		
+		<!-- Judul tab browser -->
+		<title>Motif Batik Ondel-Ondel - KBBI</title>
+		
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" >
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+         
+        <!-- Style CSS -->
+        <link href="css/style.css" rel="stylesheet">
+		
+		<!-- Font -->
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
+    </head>
+		<?php
+			require('./header.php');
+		?>
+    <body>
+        <div class="ondel-container">
+			<div class="gambar-tepi-flip">
+				<img src="img/icon/ikonpink.png" alt="pinkikon"/>
+			</div>
+			<div class="konten-ondeljudul">
+				BATIK ONDEL-ONDEL
+				<div class="konten-ondelisi">
+					</br>
+					</br>
+					</br>
+					Motif Ondel-Ondel pada Batik Betawi diangkat dari peranan Ondel-Ondel yang digunakan sebagai boneka penolak bala. Ondel-Ondel dibuat untuk mendapatkan kehidupan yang tentram dan jauh dari bala bencana. Dalam batik ini Ondel-Ondel digambarkan sebagai boneka besar none dan abang Jakarta dengan warna-warni mencolok, akan tetapi pada batik ini cukup dibuat sketsa gambar kecil yang disesuaikan dengan karakter ragam hias batik. Pada batik ini terdapat objek diantaranya patung selamat datang, dibawahnya terdapat gambar pohon. Batik ini sering digunakan pada  acara adat Betawi.
+				</div>
+			</div>
+			<div class="foto-kanan-ondel">
+				<img src="img/web/ondelondel.png" alt="about" height="100%" width="100%"/>
+			</div>
+        </div>
+		
+		<!-- JS -->
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery-3.1.1.js"></script>
+		<script src="js/carousel.js"></script>
+    </body>
+</html>
