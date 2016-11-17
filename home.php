1ec9db53c568df7b0a549d0fce69a12bf74be8fb
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-17 16:59:21 +0000

    Update home.php

--- home.php
+++ home.php
@@ -1,24 +1,7 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		
-		<!-- Gambar tab browser -->
-		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
-		
 		<!-- Judul tab browser -->
 		<title>Home - KBBI</title>
-		
-        <!-- Bootstrap CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" >
-        <link href="css/bootstrap-theme.css" rel="stylesheet">
-         
-        <!-- Style CSS -->
-        <link href="css/style.css" rel="stylesheet">
-		
-		<!-- Font -->
-		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
     </head>
 		<?php
 			require('./header.php');
@@ -36,13 +19,13 @@
 				<!-- Wrapper for slides -->
 				<div class="carousel-inner" role="listbox">
 					<div class="item active">
-						<img src="img/web/homepage1.png" alt="gambar1" height="100%">
+						<img src="./img/web/homepage1.png" alt="gambar1" height="100%">
 					</div>
 					<div class="item">
-						<img src="img/web/homepage2.png" alt="gambar2">
+						<img src="./img/web/homepage2.png" alt="gambar2">
 					</div>
 					<div class="item">
-						<img src="img/web/homepage3.png" alt="gambar3">
+						<img src="./img/web/homepage3.png" alt="gambar3">
 					</div>
 				</div>
 
@@ -59,8 +42,8 @@
         </div>
 		
 		<!-- JS -->
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/jquery-3.1.1.js"></script>
-		<script src="js/carousel.js"></script>
+		<script src="./js/bootstrap.min.js"></script>
+		<script src="./js/jquery-3.1.1.js"></script>
+		<script src="./js/carousel.js"></script>
     </body>
 </html>
