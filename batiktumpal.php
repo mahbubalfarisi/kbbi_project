eeba1448dc86eb169625d179da9930867f153018
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-17 16:50:46 +0000

    Update batiktumpal.php

--- batiktumpal.php
+++ batiktumpal.php
@@ -1,33 +1,15 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		
-		<!-- Gambar tab browser -->
-		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
-		
 		<!-- Judul tab browser -->
 		<title>Batik Tumpal Betawi - KBBI</title>
-		
-        <!-- Bootstrap CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" >
-        <link href="css/bootstrap-theme.css" rel="stylesheet">
-         
-        <!-- Style CSS -->
-        <link href="css/style.css" rel="stylesheet">
-		
-		<!-- Font -->
-		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
     </head>
 		<?php
 			require('./header.php');
 		?>
-	
     <body>
         <div class="tumpal-container">
 			<div class="gambar-tepi">
-				<img src="img/icon/ikonyellow.png" alt="yellowikon"/>
+				<img src="./img/icon/ikonyellow.png" alt="yellowikon"/>
 			</div>
 			<div class="konten-tumpaljudul">
 				BATIK TUMPAL BETAWI
@@ -39,13 +21,13 @@
 				</div>
 			</div>
 			<div class="foto-kanan-tumpal">
-				<img src="img/web/tumpalbetawi.png" alt="about" height="100%" width="100%"/>
+				<img src="./img/web/tumpalbetawi.png" alt="about" height="100%" width="100%"/>
 			</div>
         </div>
 		
 		<!-- JS -->
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/jquery-3.1.1.js"></script>
-		<script src="js/carousel.js"></script>
+		<script src="./js/bootstrap.min.js"></script>
+		<script src="./js/jquery-3.1.1.js"></script>
+		<script src="./js/carousel.js"></script>
     </body>
 </html>
