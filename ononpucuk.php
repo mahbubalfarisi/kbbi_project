a53ec17263a398a0f63a6d889b6e4d0ebd329b30
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-17 17:03:36 +0000

    Update ononpucuk.php

--- ononpucuk.php
+++ ononpucuk.php
@@ -1,33 +1,15 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		
-		<!-- Gambar tab browser -->
-		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
-		
 		<!-- Judul tab browser -->
 		<title>Motif Ondel-Ondel Pucuk Rebung - KBBI</title>
-		
-        <!-- Bootstrap CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" >
-        <link href="css/bootstrap-theme.css" rel="stylesheet">
-         
-        <!-- Style CSS -->
-        <link href="css/style.css" rel="stylesheet">
-		
-		<!-- Font -->
-		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
     </head>
 		<?php
 			require('./header.php');
 		?>
-	
     <body>
         <div class="oopr-container">
 			<div class="gambar-tepi">
-				<img src="img/icon/ikonpink.png" alt="pinkikon"/>
+				<img src="./img/icon/ikonpink.png" alt="pinkikon"/>
 			</div>
 			<div class="konten-ooprjudul">
 				BATIK ONDEL-ONDEL </br>
@@ -40,13 +22,13 @@
 				</div>
 			</div>
 			<div class="foto-kanan-oopr">
-				<img src="img/web/oopr.png" alt="about" height="100%" width="100%"/>
+				<img src="./img/web/oopr.png" alt="about" height="100%" width="100%"/>
 			</div>
         </div>
 		
 		<!-- JS -->
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/jquery-3.1.1.js"></script>
-		<script src="js/carousel.js"></script>
+		<script src="./js/bootstrap.min.js"></script>
+		<script src="./js/jquery-3.1.1.js"></script>
+		<script src="./js/carousel.js"></script>
     </body>
 </html>
