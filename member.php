b84b2ce8046056c413ed6e4a9fa9fdb0c8118956
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-17 16:59:49 +0000

    Update member.php

--- member.php
+++ member.php
@@ -1,38 +1,20 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		
-		<!-- Gambar tab browser -->
-		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
-		
 		<!-- Judul tab browser -->
 		<title>Member - KBBI</title>
-		
-        <!-- Bootstrap CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" >
-        <link href="css/bootstrap-theme.css" rel="stylesheet">
-         
-        <!-- Style CSS -->
-        <link href="css/style.css" rel="stylesheet">
-		
-		<!-- Font -->
-		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
     </head>
 		<?php
 			require('./header.php');
 		?>
-	
     <body>
         <div class="member-container">
 			<div class="gambar-tepi">
-				<img src="img/icon/ikonpink.png" alt="pinkikon"/>
+				<img src="./img/icon/ikonpink.png" alt="pinkikon"/>
 			</div>
 			<div class="konten-memberjudul">
 				MEMBER KAMI
-				<div class="logomember">
-					<img src="img/logo/terogong.png" alt="member1" height="100%" width="50%"/>
+				<div class="logomember1">
+					<img src="./img/logo/terogong.png" alt="member1" height="20%" width="50%"/>
 				</div>
 				<div class="konten-memberjuduljudul">
 					</br>
@@ -53,19 +35,19 @@
 						Jl. Terogong 3, RT.9/RW.10, Cilandak Bar. </br>
 						Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12430 </br>
 						Phone: (021) 7664672 </br>
-						<a href="#" style="color:#000">www.batikbetawiterogong.com</a></br>
+						<a href="http://batikbetawiterogong.com/" style="color:#000">www.batikbetawiterogong.com</a></br>
 						<a class="buttonnext" href="member2"><span>Next </span></a>
 					</div>
 				</div>
 			</div>
 			<div class="foto-kanan-member">
-				<img src="img/web/member1.png" alt="about" height="100%" width="100%"/>
+				<img src="./img/web/member1.png" alt="about" height="100%" width="100%"/>
 			</div>
         </div>
 		
 		<!-- JS -->
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/jquery-3.1.1.js"></script>
-		<script src="js/carousel.js"></script>
+		<script src="./js/bootstrap.min.js"></script>
+		<script src="./js/jquery-3.1.1.js"></script>
+		<script src="./js/carousel.js"></script>
     </body>
 </html>
