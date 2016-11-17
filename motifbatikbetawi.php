b38f299836c7d173448ebac7a144831b5eda41a9
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-17 17:02:52 +0000

    Update motifbatikbetawi.php

--- motifbatikbetawi.php
+++ motifbatikbetawi.php
@@ -1,24 +1,7 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		
-		<!-- Gambar tab browser -->
-		<link rel='shortcut icon' href="img/favicon.ico" type='image/x-icon'/>
-		
 		<!-- Judul tab browser -->
 		<title>Motif Batik Betawi - KBBI</title>
-		
-        <!-- Bootstrap CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" >
-        <link href="css/bootstrap-theme.css" rel="stylesheet">
-         
-        <!-- Style CSS -->
-        <link href="css/style.css" rel="stylesheet">
-		
-		<!-- Font -->
-		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Quicksand" rel="stylesheet">
     </head>
 		<?php
 			require('./header.php');
@@ -26,7 +9,7 @@
     <body>
         <div class="motifbb-container">
 			<div class="gambar-tepi">
-				<img src="img/icon/ikonpink.png" alt="pinkikon"/>
+				<img src="./img/icon/ikonpink.png" alt="pinkikon"/>
 			</div>
 			<div class="konten-motifbbjudul">
 				<h1> MOTIF </h1>
@@ -41,13 +24,13 @@
 				</div>
 			</div>
 			<div class="foto-kanan-motifbb">
-				<img src="img/web/motifbatikbetawi.png" alt="about" height="100%" width="100%"/>
+				<img src="./img/web/motifbatikbetawi.png" alt="about" height="100%" width="100%"/>
 			</div>
         </div>
 		
 		<!-- JS -->
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/jquery-3.1.1.js"></script>
-		<script src="js/carousel.js"></script>
+		<script src="./js/bootstrap.min.js"></script>
+		<script src="./js/jquery-3.1.1.js"></script>
+		<script src="./js/carousel.js"></script>
     </body>
 </html>
