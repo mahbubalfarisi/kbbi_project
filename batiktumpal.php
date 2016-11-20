6eb359c8576466ff7428e621e5dfb26d64db77c4
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:07:28 +0000

    Update batiktumpal.php

--- batiktumpal.php
+++ batiktumpal.php
@@ -3,31 +3,28 @@
 		<!-- Judul tab browser -->
 		<title>Batik Tumpal Betawi - KBBI</title>
     </head>
-		<?php
-			require('./header.php');
-		?>
+	<?php
+		require('./header.php');
+	?>
     <body>
-        <div class="tumpal-container">
-			<div class="gambar-tepi">
-				<img src="./img/icon/ikonyellow.png" alt="yellowikon"/>
+        <div class="container kbbi-con white-con">
+			<img class="yellow-segitiga" src="./img/icon/ikonyellow.png" alt="yellowikon"/>
+			<div class="main tumpal-main">
+				<h1 class="green-title"> BATIK TUMPAL BETAWI </h1>
+				<p class="tumpal-def">
+						Batik motif Tumpal Betawi merupakan batik kebanggaan bagi orang Betawi. Batik ini terbagi menjadi tiga bidang yaitu,
+						dibidang tengah diisi dengan ragam hias tumpal yang terpadu. Pada bidang kiri dan kanan diisi dengan ragam hias burung
+						seruni dan bunga warna hijau dikomposisikan dengan lung-lungan kecil-kecil, serta tumpalnya diisi bunga-bunga kecil dengan
+						latar hitam. Batik ini biasanya dipadukan dengan kebaya encim dan dipakai oleh noni-noni Betawi pada acara pernikahan.
+				</p>
 			</div>
-			<div class="konten-tumpaljudul">
-				BATIK TUMPAL BETAWI
-				<div class="konten-tumpalisi">
-					</br>
-					</br>
-					</br>
-						Batik motif Tumpal Betawi merupakan batik kebanggaan bagi orang Betawi. Batik ini terbagi menjadi tiga bidang yaitu, dibidang tengah diisi dengan ragam hias tumpal yang terpadu. Pada bidang kiri dan kanan diisi dengan ragam hias burung seruni dan bunga warna hijau dikomposisikan dengan lung-lungan kecil-kecil, serta tumpalnya diisi bunga-bunga kecil dengan latar hitam. Batik ini biasanya dipadukan dengan kebaya encim dan dipakai oleh noni-noni Betawi pada acara pernikahan.
-				</div>
-			</div>
-			<div class="foto-kanan-tumpal">
-				<img src="./img/web/tumpalbetawi.png" alt="about" height="100%" width="100%"/>
+			<div class="tumpal-foto-kanan">
+				<img src="./img/web/tumpalbetawi.png" alt="about"/>
 			</div>
         </div>
 		
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
 		<script src="./js/jquery-3.1.1.js"></script>
-		<script src="./js/carousel.js"></script>
     </body>
 </html>
