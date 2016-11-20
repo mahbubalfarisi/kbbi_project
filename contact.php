73ba014f1737ca54037713367ad4c5d0ceb79d17
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:08:22 +0000

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,32 +3,27 @@
 		<!-- Judul tab browser -->
 		<title>Kontak Kami - KBBI</title>
     </head>
-		<?php
-			require('./header.php');
-		?>
+	<?php
+		require('./header.php');
+	?>
     <body>
-        <div class="contact-container">
-			<div class="gambar-tepi">
-				<img src="./img/icon/ikonyellow.png" alt="yellowikon"/>
+        <div class="container kbbi-con white-con">
+			<img class="yellow-segitiga" src="./img/icon/ikonyellow.png" alt="yellowikon"/>
+			<div class="main contact-main">
+				<h1 class="pink-title"> KONTAK KAMI </h1>
+				<p class="contact-def">
+					WORKSHOP : </br>
+					Kp. Budaya Betawi Setu Babakan Jl. Kramat bambu No. 77 RT. 012/RW. 008 Kelurahan Srengseng Sawah Kecamatan Jagakarsa
+					</br></br> PHONE :</br>0811 903 870, 0852 1740 5011, 0895 3390 25897
+				</p>
 			</div>
-			<div class="konten-contactjudul">
-				KONTAK KAMI
-				<div class="konten-contactisi">
-					</br>
-					</br>
-					</br>
-						WORKSHOP : </br>
-						Kp. Budaya Betawi Setu Babakan Jl. Kramat bambu No. 77 RT. 012/RW. 008 Kelurahan Srengseng Sawah Kecamatan Jagakarsa </br></br> PHONE :</br>0811 903 870,  0852 1740 5011, 0895 3390 25897
-				</div>
-			</div>
-			<div class="foto-kanan-contact">
-				<img src="./img/web/contact.png" alt="about" height="100%" width="100%"/>
+			<div class="contact-foto-kanan">
+				<img src="./img/web/contact.png" alt="about"/>
 			</div>
         </div>
 		
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
 		<script src="./js/jquery-3.1.1.js"></script>
-		<script src="./js/carousel.js"></script>
     </body>
 </html>
