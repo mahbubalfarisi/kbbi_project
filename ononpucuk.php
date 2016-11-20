9dd435bc9e1b3f877efe1f1af54456df5f5f5924
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:23:41 +0000

    Update ononpucuk.php

--- ononpucuk.php
+++ ononpucuk.php
@@ -1,34 +1,29 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-		<!-- Judul tab browser -->
 		<title>Motif Ondel-Ondel Pucuk Rebung - KBBI</title>
     </head>
-		<?php
-			require('./header.php');
-		?>
+	<?php
+		require('./header.php');
+	?>
     <body>
-        <div class="oopr-container">
-			<div class="gambar-tepi">
-				<img src="./img/icon/ikonpink.png" alt="pinkikon"/>
+        <div class="container kbbi-con pink-con">
+			<img class="pink-segitiga" src="./img/icon/ikonpink.png" alt="pinkikon"/>
+			<div class="main oopr-main">
+				<h1 class="yellow-title"> BATIK ONDEL-ONDEL </h1>
+				<h1 class="yellow-title"> PUCUK REBUNG </h1>
+				<p class="oopr-def">
+					Batik Betawi Motif Pucuk Rebung merupakan khas batik pesisir yang menggambarkan “Pucung Batang Bambu”. Motif ini merupakan
+					perpaduan motif modern ondel-ondel dan motif tradisional pucuk rebung. Batik ini digunakan sebagai seragam wajib ketika
+					pemilihan Abang-None dan digunakan sebagai busana bawahan none.
+				</p>
 			</div>
-			<div class="konten-ooprjudul">
-				BATIK ONDEL-ONDEL </br>
-				PUCUK REBUNG
-				<div class="konten-ooprisi">
-					</br>
-					</br>
-					</br>
-						Batik Betawi Motif Pucuk Rebung merupakan khas batik pesisir yang menggambarkan “Pucung Batang Bambu”. Motif ini merupakan perpaduan motif modern ondel-ondel dan motif tradisional pucuk rebung. Batik ini digunakan sebagai seragam wajib ketika pemilihan Abang-None dan digunakan sebagai busana bawahan none.
-				</div>
-			</div>
-			<div class="foto-kanan-oopr">
-				<img src="./img/web/oopr.png" alt="about" height="100%" width="100%"/>
+			<div class="oopr-foto-kanan">
+				<img src="./img/web/oopr.png" alt="about"/>
 			</div>
         </div>
 		
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
 		<script src="./js/jquery-3.1.1.js"></script>
-		<script src="./js/carousel.js"></script>
     </body>
 </html>
