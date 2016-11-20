49d3d8beb2f9cb1394c9a377b7f6d097db2e35c6
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 17:11:01 +0000

    Update event.php

--- event.php
+++ event.php
@@ -1,13 +1,43 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-		<!-- Judul tab browser -->
 		<title>Event - KBBI</title>
     </head>
 	<?php
 		require('./header.php');
 	?>
     <body>
-		<img class="event-foto-kanan" src="./img/web/event.png" alt="about"/>
+    	<div class="row">
+			<img class="event-foto-kanan" src="./img/web/event.png" alt="about"/>
+		</div>
+		<div class="row">
+			<div class="container event-con">
+				<div class="textbox">
+					<h3 class="event-def">
+						TALKSHOW & WORKSHOP
+					</h3>
+					<h4 class="event-def">
+						Sonny Muclishon dan Anne Avantie </br>
+						''Batik Betawi Punya Cerite''
+					</h4>
+					<h6 class="event-def">
+						15 Januari 2017 </br>
+						14.00 PM-15.00 PM
+					</h6>
+				</div>
+				<div class="textbox2">
+					<h3 class="event-def">
+						EXHIBITION
+					</h3>
+					<h4 class="event-def">
+						''Batik Betawi Kuno''
+					</h4>
+					<h6 class="event-def">
+						15-16 Januari 2017 </br>
+						10.00 AM-22.OO PM
+					</h6>
+				</div>
+			</div>
+		</div>
 		
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
