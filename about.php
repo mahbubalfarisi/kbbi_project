53c65a3dea6c10a799fab0dc6dfa8da858e60674
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:01:13 +0000

    Update about.php

--- about.php
+++ about.php
@@ -1,33 +1,28 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>About - KBBI</title>
+		<title>Tentang Kami - KBBI</title>
 	</head>
-		<?php
-			require('./header.php');
-		?>
+	<?php
+		require('./header.php');
+	?>
     <body>
-        <div class="container-konten">
-			<div class="gambar-tepi">
-				<img src="./img/icon/ikonpink.png" alt="pinkikon"/>
-		</div>
-			<div class="konten-about">
-				<h1> ABOUT US </h1>
-				<p>Keluarga Batik Betawi adalah sebuah himpunan pengrajin 
-						Batik Betawi yang berkeinginan untuk mengenalkan kebudayaan
-						batik khas Betawi kepada masyarakat luas, baik dari pelatihan untuk
-						menumbuhkan pengrajin generasi berikutnya  atau melalui
-						pameran-pameran. Masyarakat dapat belajar 
-						tentang Batik Betawi dan proses pembuatan Batik Betawi.
+        <div class="container kbbi-con pink-con">
+			<img class="pink-segitiga" src="./img/icon/ikonpink.png" alt="pinkikon"/>
+			<div class="main about-main">
+				<h1 class="pink-title"> TENTANG KAMI </h1>
+				<p class="about-def">
+					Keluarga Batik Betawi adalah sebuah himpunan pengrajin Batik Betawi yang berkeinginan untuk mengenalkan kebudayaan batik khas Betawi
+					kepada masyarakat luas, baik dari pelatihan untuk menumbuhkan pengrajin generasi berikutnya  atau melalui pameran-pameran. Masyarakat
+					dapat belajar tentang Batik Betawi dan proses pembuatan Batik Betawi.
 				</p>
 			</div>
-			<div class="foto-kanan">
-				<img src="./img/web/tentangkami.png" alt="about" height="100%" width="100%"/>
+			<div class="about-foto-kanan">
+				<img src="./img/web/tentangkami.png" alt="about"/>
 			</div>
         </div>
 		
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
 		<script src="./js/jquery-3.1.1.js"></script>
-		<script src="./js/carousel.js"></script>
     </body>
 </html>
