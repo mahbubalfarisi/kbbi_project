a530e5c018d6d0df13c9ecc5979bf6957f5494fc
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:06:31 +0000

    Update batikonon.php

--- batikonon.php
+++ batikonon.php
@@ -3,31 +3,29 @@
 		<!-- Judul tab browser -->
 		<title>Motif Batik Ondel-Ondel - KBBI</title>
     </head>
-		<?php
-			require('./header.php');
-		?>
+	<?php
+		require('./header.php');
+	?>
     <body>
-        <div class="ondel-container">
-			<div class="gambar-tepi-flip">
-				<img src="./img/icon/ikonpink.png" alt="pinkikon"/>
+        <div class="container kbbi-con pink-con">
+			<img class="pink-segitiga-flip" src="./img/icon/ikonpink.png" alt="pinkikon"/>
+			<div class="main ondel-main">
+				<h1 class="pink-title"> BATIK ONDEL-ONDEL </h1>
+				<p class="ondel-def">
+					Motif Ondel-Ondel pada Batik Betawi diangkat dari peranan Ondel-Ondel yang digunakan sebagai boneka penolak bala. Ondel-Ondel
+					dibuat untuk mendapatkan kehidupan yang tentram dan jauh dari bala bencana. Dalam batik ini Ondel-Ondel digambarkan sebagai
+					boneka besar none dan abang Jakarta dengan warna-warni mencolok, akan tetapi pada batik ini cukup dibuat sketsa gambar kecil
+					yang disesuaikan dengan karakter ragam hias batik. Pada batik ini terdapat objek diantaranya patung selamat datang, dibawahnya
+					terdapat gambar pohon. Batik ini sering digunakan pada  acara adat Betawi.
+				</p>
 			</div>
-			<div class="konten-ondeljudul">
-				BATIK ONDEL-ONDEL
-				<div class="konten-ondelisi">
-					</br>
-					</br>
-					</br>
-					Motif Ondel-Ondel pada Batik Betawi diangkat dari peranan Ondel-Ondel yang digunakan sebagai boneka penolak bala. Ondel-Ondel dibuat untuk mendapatkan kehidupan yang tentram dan jauh dari bala bencana. Dalam batik ini Ondel-Ondel digambarkan sebagai boneka besar none dan abang Jakarta dengan warna-warni mencolok, akan tetapi pada batik ini cukup dibuat sketsa gambar kecil yang disesuaikan dengan karakter ragam hias batik. Pada batik ini terdapat objek diantaranya patung selamat datang, dibawahnya terdapat gambar pohon. Batik ini sering digunakan pada  acara adat Betawi.
-				</div>
-			</div>
-			<div class="foto-kanan-ondel">
-				<img src="./img/web/ondelondel.png" alt="about" height="100%" width="100%"/>
+			<div class="ondel-foto-kiri">
+				<img src="./img/web/ondelondel.png" alt="about"/>
 			</div>
         </div>
 		
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
 		<script src="./js/jquery-3.1.1.js"></script>
-		<script src="./js/carousel.js"></script>
     </body>
 </html>
