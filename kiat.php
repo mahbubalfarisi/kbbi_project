d60eaa9eb3e56bf750c14adc8b21748122d77529
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:17:54 +0000

    Update kiat.php

--- kiat.php
+++ kiat.php
@@ -7,96 +7,91 @@
 			require('./header.php');
 		?>
     <body>
-        <div class="tutor-container">
-			<div class="gambar-tepirel">
-				<img src="./img/icon/ikonpink.png" alt="pinkikon"/>
-			</div>
+        <div class="container kbbi-con pink-con">
+			<img class="pink-segitiga-rel" src="./img/icon/ikonpink.png" alt="pinkikon"/>
 			
 			<!-- Baris pertama -->
 			<div class="row">
 				<div class="col-md-12">
-					<div class="konten-tutorjudul">
-						<h1> KIAT BERKAIN CANTIK </h1>
-						<div class="konten-tutorisi">
-							</br>
-							</br>
-							</br>
-								Berkebaya yang serasi dapat tercipta dari pemilihan model kain yang dapat disesuaikan dengan kebaya dan batik yang dikenakakan. Batik Betawi mempunyai berberapa kiat agar kain terlihat cantik dan menarik.
-						</div>
+					<div class="main kiat-main">
+						<h1 class="pink-title"> KIAT BERKAIN CANTIK </h1>
+						<p class="kiat-def">
+							Berkebaya yang serasi dapat tercipta dari pemilihan model kain yang dapat disesuaikan dengan kebaya dan batik yang
+							dikenakakan. Batik Betawi mempunyai berberapa kiat agar kain terlihat cantik dan menarik.
+						</p>
 					</div>
 				</div>
 			</div>
-			<div class="foto-kanan-tutor">
-				<img src="./img/web/tutor.png" alt="about" height="100%" width="100%"/>
+			<div class="kiat-foto-kanan">
+				<img src="./img/web/tutor.png" alt="about"/>
 			</div>
 			
 			<!-- Baris kedua -->
 			<div class="row">
 				<div class="col-md-12">
-					<div class="konten-kiat2isi">
-							Bentangkan kain dibagian belakang. Pegang ujung kain sisi kanan.                                        
-					</div>
-					<div class="konten-kiat2isi2">
-							Bawa ujung kain sisi kiri, dan pertemukan kedua ujung kain. Pegang kuat dengan pinggul.                                       
-					</div>
-					<div class="konten-kiat2isi3">
-							Ikat pangkal lain. Kenakan step in dan atur agar jatuhan kain membentuk draperi.                                      
-					</div>
-					<div class="foto-tutor11">
+					<p class="kiat-atas-isi-11">
+						Bentangkan kain dibagian belakang. Pegang ujung kain sisi kanan.                                        
+					</p>
+					<p class="kiat-atas-isi-12">
+						Bawa ujung kain sisi kiri, dan pertemukan kedua ujung kain. Pegang kuat dengan pinggul.                                       
+					</p>
+					<p class="kiat-atas-isi-13">
+						Ikat pangkal lain. Kenakan step in dan atur agar jatuhan kain membentuk draperi.                                      
+					</p>
+					<div class="kiat-foto kiat-atas-foto-11">
 						<img src="./img/web/tutor-11.png" alt="about" height="60%" width="40%"/>
 					</div>
-					<div class="foto-tutor12">
+					<div class="kiat-foto kiat-atas-foto-12">
 						<img src="./img/web/tutor-12.png" alt="about" height="80%" width="48%"/>
 					</div>
-					<div class="foto-tutor13">
+					<div class="kiat-foto kiat-atas-foto-13">
 						<img src="./img/web/tutor-13.png" alt="about" height="65%" width="76%"/>
 					</div>
-					<div class="ikon-tutor11">
-						<img src="./img/icon/ikon11.png" alt="about" height="100%" width="100%"/>
+					<div class="kiat-ikon kiat-ikon-11">
+						<img src="./img/icon/ikon11.png" alt="about"/>
 					</div>
-					<div class="ikon-tutor12">
-						<img src="./img/icon/ikon12.png" alt="about" height="100%" width="100%"/>
+					<div class="kiat-ikon kiat-ikon-12">
+						<img src="./img/icon/ikon12.png" alt="about"/>
 					</div>
-					<div class="ikon-tutor13">
-						<img src="./img/icon/ikon13.png" alt="about" height="100%" width="100%"/>
+					<div class="kiat-ikon kiat-ikon-13">
+						<img src="./img/icon/ikon13.png" alt="about"/>
 					</div>
-					<div class="foto-kanan-kiat-flip">
-						<img src="./img/web/tutor1.png" alt="about" height="100%" width="100%"/>
+					<div class="kiat-foto-kiri">
+						<img src="./img/web/tutor1.png" alt="about"/>
 					</div>
 				</div>
 			</div>
 			
 			<!-- Baris ketiga -->
 			<div class="row">
-				<div class="kiat-bawah-container">
-					<div class="kiat-bawah-isi">
-							Siapkan kain dengan posisi memanjang depan dan kebelakang. Ambil ujung sisi kiri ke belakang dan ikuti kanan kain ke belakang. Ikat kuat.                                        
+				<div class="container kiat-bawah-con pink-con">
+					<p class="kiat-bawah-isi-21">
+						Siapkan kain dengan posisi memanjang depan dan kebelakang. Ambil ujung sisi kiri ke belakang dan ikuti kanan kain ke belakang.
+						Ikat kuat.                                        
+					</p>
+					<p class="kiat-bawah-isi-22">
+						Ambil ujung-ujung kain yang ada dibakang, bawa ke arah pinggang, lalu tarik ujung-ujungnya ke sisi kiri dan ikat lalu rapikan.                 
+					</p>
+					<div class="kiat-foto foto-kiat-bawah-21">
+						<img src="./img/web/tutor-21.png" alt="about" height="75%" width="55%"/>
 					</div>
-					<div class="kiat-bawah-isi2">
-							Ambil ujung-ujung kain yang ada dibakang, bawa ke arah pinggang, lalu tarik ujung-ujungnya ke sisi kiri dan ikat lalu rapikan.                 
-					</div>
-					<div class="foto-kiat-bawah11">
-						<img src="./img/web/tutor-21.png" alt="about" height="65%" width="45%"/>
-					</div>
-					<div class="foto-kiat-bawah12">
+					<div class="kiat-foto foto-kiat-bawah-22">
 						<img src="./img/web/tutor-22.png" alt="about" height="68%" width="47%"/>
 					</div>
-					<div class="ikon-kiat-bawah11">
-						<img src="./img/icon/ikon11.png" alt="about" height="100%" width="100%"/>
+					<div class="kiat-ikon kiat-ikon-21">
+						<img src="./img/icon/ikon11.png" alt="about"/>
 					</div>
-					<div class="ikon-kiat-bawah12">
-						<img src="./img/icon/ikon12.png" alt="about" height="100%" width="100%"/>
+					<div class="kiat-ikon kiat-ikon-22">
+						<img src="./img/icon/ikon12.png" alt="about"/>
 					</div>
-					<div class="foto-kanan-kiat-bawah">
-						<img src="./img/web/tutor2.png" alt="about" height="100%" width="100%"/>
+					<div class="kiat-foto-kanan-bawah">
+						<img src="./img/web/tutor2.png" alt="about"/>
 					</div>
 				</div>
 			</div>
-			
-		
+		</div>		
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
 		<script src="./js/jquery-3.1.1.js"></script>
-		<script src="./js/carousel.js"></script>
     </body>
 </html>
