b32e9543b425a1ec189a37c94271e75871aa3843
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:15:25 +0000

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
+		<!-- JS -->
+		<script src="./js/search.js"></script>
+		<script src="./js/bootstrap.min.js"></script>
+		<script src="./js/jquery.min.js" charset="utf-8"></script>
+   		<script src="./js/renderedfont-0.8.min.js" charset="utf-8"></script>
+   		
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<!-- Menyesuaikan layar -->
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,43 +22,43 @@
     </head>
 	
     <body>
-		<div class="header-container navbar-fixed-top">
+		<div class="container header-con navbar-fixed-top">
 			<nav class="navbar">
 				<div class="row">
-					<div class="col-md-2">
+					<div class="col-xs-3 col-sm-2">
 						<div class="header-logo">
 							<a href="./home">
 								<img src="./img/logo/logoteks.png" class="img-responsive" alt="logo-kbbi" />
 							</a>
 						</div>
 					</div>
-					<div class="col-md-7">
+					<div class="col-xs-9 col-sm-6">
 						<div class="menu-header">
 							<div class="collapse navbar-collapse" id="myNavbar">
 								<ul class="nav navbar-nav">
 									<li><a href="./home">BERANDA</a></li>
-									<li>
-										<div class="dropdown">
-											<button class="dropbtn">TENTANG KAMI</button>
-											<div class="dropdown-content">
-												<a href="./about">Tentang KBBI</a>
-												<a href="./sejbatikbetawi">Sejarah Batik Betawi</a>
-												<a href="./motifbatikbetawi">Motif Batik Betawi</a>
-												<a href="./kiat">Kiat Berkain Cantik</a>
-												<a href="./contact">Kontak Kami</a>
-											</div>
+									<li role="presentation" class="dropdown">
+										<a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+											TENTANG KAMI <span class="caret"></span>
+										</a>
+   										<div class="dropdown-content">
+											<a href="./about">Tentang KBBI</a>
+											<a href="./sejbatikbetawi">Sejarah Batik Betawi</a>
+											<a href="./motifbatikbetawi">Motif Batik Betawi</a>
+											<a href="./kiat">Kiat Berkain Cantik</a>
+											<a href="./contact">Kontak Kami</a>
 										</div>
 									</li>
-									<li>
-										<div class="dropdown">
-											<button class="dropbtn">GALERI</button>
-											<div class="dropdown-content2">
-												<a href="./batikmonas">Motif Batik Monas</a>
-												<a href="./batikonon">Motif Batik Ondel-Ondel</a>
-												<a href="./batiktumpal">Motif Tumpal Betawi</a>
-												<a href="./gunungtumpal">Motif Gunung Tumpal</a>
-												<a href="./ononpucuk">Motif Ondel-Ondel Pucuk Rebung</a>
-											</div>
+									<li role="presentation" class="dropdown">
+										<a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+											GALERI <span class="caret"></span>
+										</a>
+										<div class="dropdown-content">
+											<a href="./batikmonas">Motif Batik Monas</a>
+											<a href="./batikonon">Motif Batik Ondel-Ondel</a>
+											<a href="./batiktumpal">Motif Tumpal Betawi</a>
+											<a href="./gunungtumpal">Motif Gunung Tumpal</a>
+											<a href="./ononpucuk">Motif Ondel-Ondel Pucuk Rebung</a>
 										</div>
 									</li>
 									<li><a href="./event">EVENT</a></li>
@@ -61,7 +67,7 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-md-1">
+					<div class="col-xs-3 col-sm-1">
 						<div class="social-header">
 							<a href="#" class="btn-social">
 								<img src="./img/icon/twitlogo.png" class="img-rounded" alt="Twitter Button" />
@@ -71,9 +77,9 @@
 							</a>
 						</div>
 					</div>
-					<div class="col-md-2">
+					<div class="col-xs-3 col-sm-3">
 						<div class="search-header">
-							<form method="get" action="http://www.google.com">
+							<form method="get" action="http://www.google.com" target="blank" role="search">
 								<div class="input-group stylish-input-group">
 									<input type="text" class="form-control"  placeholder="Search" >
 										<span class="input-group-addon">
@@ -89,9 +95,5 @@
 				</div>
 			</nav>
 		</div>
-		<!-- JS -->
-		<script src="./js/search.js"></script>
-		<script src="./js/bootstrap.min.js"></script>
-		<script src="./js/jquery-3.1.1.js"></script>
     </body>
 </html>
