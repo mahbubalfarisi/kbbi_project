056e21108e5214c69ad8e5874fcd5d5e3842ba96
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:11:44 +0000

    Update event.php

--- event.php
+++ event.php
@@ -3,19 +3,14 @@
 		<!-- Judul tab browser -->
 		<title>Event - KBBI</title>
     </head>
-		<?php
-			require('./header.php');
-		?>
+	<?php
+		require('./header.php');
+	?>
     <body>
-        <div class="event-container">
-			<div class="foto-kanan-event">
-				<img src="./img/web/event.png" alt="about" height="100%" width="100%"/>
-			</div>
-        </div>
+		<img class="event-foto-kanan" src="./img/web/event.png" alt="about"/>
 		
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
 		<script src="./js/jquery-3.1.1.js"></script>
-		<script src="./js/carousel.js"></script>
     </body>
 </html>
