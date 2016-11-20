ad8516d070fa39397e11866bd5a785fcc03e26b5
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:20:49 +0000

    Update member.php

--- member.php
+++ member.php
@@ -1,53 +1,33 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-		<!-- Judul tab browser -->
 		<title>Member - KBBI</title>
     </head>
-		<?php
-			require('./header.php');
-		?>
+	<?php
+		require('./header.php');
+	?>
     <body>
-        <div class="member-container">
-			<div class="gambar-tepi">
-				<img src="./img/icon/ikonpink.png" alt="pinkikon"/>
-			</div>
-			<div class="konten-memberjudul">
-				MEMBER KAMI
-				<div class="logomember1">
-					<img src="./img/logo/terogong.png" alt="member1" height="20%" width="50%"/>
+        <div class="container kbbi-con pink-con">
+			<img class="pink-segitiga" src="./img/icon/ikonpink.png" alt="pinkikon"/>
+			<div class="main member-main">
+				<h1 class="pink-title"> MEMBER KAMI </h1>
+				<div class="logomember">
+					<img src="./img/logo/terogong.png" alt="member1"/>
 				</div>
-				<div class="konten-memberjuduljudul">
-					</br>
-					</br>
-					</br>
-					</br>
-					</br>
-					</br>
-						 BATIK BETAWI TEROGONG
-					 <div class="konten-memberisi">
-						</br>
-						</br>
-						</br>
-						</br>
-						</br>
-						</br>
-						</br>
-						Jl. Terogong 3, RT.9/RW.10, Cilandak Bar. </br>
-						Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12430 </br>
-						Phone: (021) 7664672 </br>
-						<a href="http://batikbetawiterogong.com/" style="color:#000">www.batikbetawiterogong.com</a></br>
-						<a class="buttonnext" href="member2"><span>Next </span></a>
-					</div>
-				</div>
+				<h5> BATIK BETAWI TEROGONG </h5>
+				<p class="member-def">
+					Jl. Terogong 3, RT.9/RW.10, Cilandak Bar. </br>
+					Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12430 </br>
+					Phone: (021) 7664672 </br>
+					<a href="http://batikbetawiterogong.com/">www.batikbetawiterogong.com</a></br>
+				</p>
 			</div>
-			<div class="foto-kanan-member">
-				<img src="./img/web/member1.png" alt="about" height="100%" width="100%"/>
+			<div class="member-foto-kanan">
+				<img src="./img/web/member1.png" alt="about"/>
 			</div>
         </div>
 		
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
 		<script src="./js/jquery-3.1.1.js"></script>
-		<script src="./js/carousel.js"></script>
     </body>
 </html>
