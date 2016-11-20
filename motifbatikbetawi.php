5c1bd688161aad6938c0e2003c0c78d45c8d3c1d
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:21:53 +0000

    Update motifbatikbetawi.php

--- motifbatikbetawi.php
+++ motifbatikbetawi.php
@@ -1,29 +1,23 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-		<!-- Judul tab browser -->
 		<title>Motif Batik Betawi - KBBI</title>
     </head>
-		<?php
-			require('./header.php');
-		?>
+	<?php
+		require('./header.php');
+	?>
     <body>
-        <div class="motifbb-container">
-			<div class="gambar-tepi">
-				<img src="./img/icon/ikonpink.png" alt="pinkikon"/>
+        <div class="container kbbi-con pink-con">
+			<img class="pink-segitiga" src="./img/icon/ikonpink.png" alt="pinkikon"/>
+			<div class="main motif-main">
+				<h1 class="pink-title"> MOTIF BATIK BETAWI </h1>
+				<p class="motif-def">
+					Motif Batik Betawi memiliki warna dan corak yang berbeda dari daerah lainya, yaitu adanya pengaruh lingkungan, falsafah dan adat
+					istiadat orang Betawi serta dipengaruhi oleh Cina. Batik ini mempunyai 5 jenis motif utama diantaranya Ondel-Ondel, Tumpal Betawi,
+					Pucuk Rebung, Gunung Tumpal, dan Monas. Ciri khas warna Batik Betawi mempunyai warna-warna yang mencolok seperti merah, hijau, oranye
+					dan kuning.
+				</p>
 			</div>
-			<div class="konten-motifbbjudul">
-				<h1> MOTIF </h1>
-				<h1> BATIK BETAWI </h1>
-				<div class="konten-motifbbisi">
-					</br>
-					</br>
-					</br>
-					</br>
-					</br>
-						Motif Batik Betawi memiliki warna dan corak yang berbeda dari daerah lainya, yaitu adanya pengaruh lingkungan, falsafah dan adat istiadat orang Betawi serta dipengaruhi oleh Cina. Batik ini mempunyai 5 jenis motif utama diantaranya Ondel-Ondel, Tumpal Betawi, Pucuk Rebung, Gunung Tumpal, dan Monas. Ciri khas warna Batik Betawi mempunyai warna-warna yang mencolok seperti merah, hijau, oranye dan kuning.
-				</div>
-			</div>
-			<div class="foto-kanan-motifbb">
+			<div class="motif-foto-kanan">
 				<img src="./img/web/motifbatikbetawi.png" alt="about" height="100%" width="100%"/>
 			</div>
         </div>
@@ -31,6 +25,5 @@
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
 		<script src="./js/jquery-3.1.1.js"></script>
-		<script src="./js/carousel.js"></script>
     </body>
 </html>
