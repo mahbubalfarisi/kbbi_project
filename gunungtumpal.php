24df55baa233c1e9f204a4f099bf0383cf9fa763
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:13:09 +0000

    Update gunungtumpal.php

--- gunungtumpal.php
+++ gunungtumpal.php
@@ -1,33 +1,28 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-		<!-- Judul tab browser -->
 		<title>Motif Gunung Tumpal - KBBI</title>
     </head>
-		<?php
-			require('./header.php');
-		?>
+	<?php
+		require('./header.php');
+	?>
     <body>
-        <div class="gunung-container">
-			<div class="gambar-tepi-flip">
-				<img src="./img/icon/ikonpink.png" alt="pinkikon"/>
+        <div class="container kbbi-con pink-con">
+			<img class="pink-segitiga-flip" src="./img/icon/ikonpink.png" alt="pinkikon"/>
+			<div class="main gunung-main">
+				<h1 class="pink-title"> BATIK GUNUNG TUMPAL </h1>
+				<p class="gunung-def">
+					Batik motif Gunung Tumpal merupakan batik pengaruh dari etnis melayu. Gunung Tumpal ini terinspirasi dari Gigi balang yang
+					mempunyai arti kesabaran, keuletan, kejujuran dan keberanian yang merupakan motivasi dari orang betawi. Batik ini dipadupadankan
+					dengan motif bunga melati yang melambangkan keramahan.
+				</p>
 			</div>
-			<div class="konten-gunungjudul">
-				BATIK GUNUNG TUMPAL
-				<div class="konten-gunungisi">
-					</br>
-					</br>
-					</br>
-					Batik motif Gunung Tumpal merupakan batik pengaruh dari etnis melayu. Gunung Tumpal ini terinspirasi dari Gigi balang yang mempunyai arti kesabaran, keuletan, kejujuran dan keberanian yang merupakan motivasi dari orang betawi. Batik ini dipadupadankan dengan motif bunga melati yang melambangkan keramahan.
-				</div>
-			</div>
-			<div class="foto-kanan-gunung">
-				<img src="./img/web/gunungtumpal.png" alt="about" width="64%"/>
+			<div class="gunung-foto-kanan">
+				<img src="./img/web/gunungtumpal.png" alt="about"/>
 			</div>
         </div>
 		
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
 		<script src="./js/jquery-3.1.1.js"></script>
-		<script src="./js/carousel.js"></script>
     </body>
 </html>
