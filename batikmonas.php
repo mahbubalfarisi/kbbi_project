1f26605a3dd1c39fdb38d9380d6bdc6048e78e13
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-20 10:02:12 +0000

    Update batikmonas.php

--- batikmonas.php
+++ batikmonas.php
@@ -3,31 +3,26 @@
 		<!-- Judul tab browser -->
 		<title>Motif Batik Monas - KBBI</title>
     </head>
-		<?php
-			require('./header.php');
-		?>
+	<?php
+		require('./header.php');
+	?>
     <body>
-        <div class="monas-container">
-			<div class="gambar-tepi">
-				<img src="./img/icon/ikonyellow.png" alt="yellowikon"/>
+        <div class="container kbbi-con white-con">
+			<img class="yellow-segitiga" src="./img/icon/ikonyellow.png" alt="yellowikon"/>
+			<div class="main monas-main">
+				<h1 class="yellow-title"> BATIK MONAS </h1>
+				<p class="monas-def">
+					Motif Batik Monas memiliki mempunyai corak ikon kota Jakarta yaitu Monas dan gedung perkantoran serta dipadupadankan dengan
+					ragam hias. Warna batik ini mempunyai 2 warna yaitu merah dan merah muda.
+				</p>
 			</div>
-			<div class="konten-monasjudul">
-				BATIK MONAS
-				<div class="konten-monasisi">
-					</br>
-					</br>
-					</br>
-						Motif Batik Monas memiliki mempunyai corak ikon kota Jakarta yaitu Monas dan gedung perkantoran serta dipadupadankan dengan ragam hias . Warna batik ini mempunyai 2 warna yaitu merah dan merah muda.
-				</div>
-			</div>
-			<div class="foto-kanan-monas">
-				<img src="./img/web/batikmonas.png" alt="about" height="100%" width="100%"/>
+			<div class="monas-foto-kanan">
+				<img src="./img/web/batikmonas.png" alt="about"/>
 			</div>
         </div>
 		
 		<!-- JS -->
 		<script src="./js/bootstrap.min.js"></script>
 		<script src="./js/jquery-3.1.1.js"></script>
-		<script src="./js/carousel.js"></script>
     </body>
 </html>
