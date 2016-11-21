af3c216e6fbc6f5069cf6a546c732b2404666499
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-21 12:21:37 +0000

    Upload new file

---
+++ headnav.php
@@ -0,0 +1,73 @@
+<div class="container header-con navbar-fixed-top">
+	<nav class="navbar">
+		<div class="row">
+			<div class="col-xs-3 col-sm-2">
+				<div class="header-logo">
+					<a href="./home">
+						<img src="./img/logo/logoteks.png" class="img-responsive" alt="logo-kbbi" />
+					</a>
+				</div>
+			</div>
+			<div class="col-xs-9 col-sm-6">
+				<div class="menu-header">
+					<div class="collapse navbar-collapse" id="myNavbar">
+						<ul class="nav navbar-nav">
+							<li><a href="./home">BERANDA</a></li>
+							<li role="presentation" class="dropdown">
+								<a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+									TENTANG KAMI <span class="caret"></span>
+								</a>
+									<div class="dropdown-content">
+									<a href="./about">Tentang KBBI</a>
+									<a href="./sejbatikbetawi">Sejarah Batik Betawi</a>
+									<a href="./motifbatikbetawi">Motif Batik Betawi</a>
+									<a href="./kiat">Kiat Berkain Cantik</a>
+									<a href="./contact">Kontak Kami</a>
+								</div>
+							</li>
+							<li role="presentation" class="dropdown">
+								<a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+									GALERI <span class="caret"></span>
+								</a>
+								<div class="dropdown-content">
+									<a href="./batikmonas">Motif Batik Monas</a>
+									<a href="./batikonon">Motif Batik Ondel-Ondel</a>
+									<a href="./batiktumpal">Motif Tumpal Betawi</a>
+									<a href="./gunungtumpal">Motif Gunung Tumpal</a>
+									<a href="./ononpucuk">Motif Ondel-Ondel Pucuk Rebung</a>
+								</div>
+							</li>
+							<li><a href="./event">EVENT</a></li>
+							<li><a href="./member">MEMBER KAMI</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-3 col-sm-1">
+				<div class="social-header">
+					<a href="#" class="btn-social">
+						<img src="./img/icon/twitlogo.png" class="img-rounded" alt="Twitter Button" />
+					</a>
+					<a href="#" class="btn-social">
+						<img src="./img/icon/fblogo.png" class="img-rounded" alt="Facebook Button"/>
+					</a>
+				</div>
+			</div>
+			<div class="col-xs-3 col-sm-3">
+				<div class="search-header">
+					<form method="get" action="http://www.google.com" target="blank" role="search">
+						<div class="input-group stylish-input-group">
+							<input type="text" class="form-control"  placeholder="Search" >
+								<span class="input-group-addon">
+									<button type="submit">
+										<span class="glyphicon glyphicon-search"></span>
+									</button>  
+								</span>
+							</input>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</nav>
+</div>
