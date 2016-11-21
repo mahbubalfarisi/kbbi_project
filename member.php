f9e8dfd60ac8109dace69ff3d60ceac00dcd423b
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-21 12:18:24 +0000

    Update member.php

--- member.php
+++ member.php
@@ -1,11 +1,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
 		<title>Member - KBBI</title>
+    	<?php
+			require('./scripts.php');
+		?>
     </head>
-	<?php
-		require('./header.php');
-	?>
     <body>
+	<?php
+		require('./headnav.php');
+		require('./scriptfont.php');
+	?>
         <div class="container kbbi-con">
 			<img class="pink-segitiga" src="./img/icon/ikonpink.png" alt="pinkikon"/>
 			<div class="main member-main">
@@ -23,12 +27,14 @@
 			        <a class="rig-cell" href="#">
 			            <img class="grid-size" src="./img/web/member2a.png">
 			            <span class="rig-overlay"></span>
+			            <span class="rig-text">Batik Betawi Terogong</span>
 			        </a>
 			    </li>
 			    <li>
 			        <a class="rig-cell" href="#">
 			            <img class="grid-size" src="./img/web/member3a.png">
 			            <span class="rig-overlay"></span>
+			            <span class="rig-text">Seraci Batik Betawi</span>
 			        </a>
 			    </li>
 			    <li>
@@ -55,13 +61,9 @@
 			</ul>
 			<ul class="pagination page-pos">
 			    <li><a href="#">1</a></li>
-			    <li class="disabled"><a href="#">2</a></li>
-			    <li class="disabled"><a href="#">3</a></li>
+			    <li><a href="#">2</a></li>
+			    <li><a href="#">3</a></li>
 			</ul>
         </div>
-		
-		<!-- JS -->
-		<script src="./js/bootstrap.min.js"></script>
-		<script src="./js/jquery-3.1.1.js"></script>
     </body>
 </html>
