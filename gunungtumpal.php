59b953fc45cba6e716ef4ec6128ba7275b044fe8
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2016-11-21 12:15:21 +0000

    Update gunungtumpal.php

--- gunungtumpal.php
+++ gunungtumpal.php
@@ -1,11 +1,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
 		<title>Motif Gunung Tumpal - KBBI</title>
+    	<?php
+			require('./scripts.php');
+		?>
     </head>
-	<?php
-		require('./header.php');
-	?>
     <body>
+	<?php
+		require('./headnav.php');
+		require('./scriptfont.php');
+	?>
         <div class="container kbbi-con pink-con">
 			<img class="pink-segitiga-flip" src="./img/icon/ikonpink.png" alt="pinkikon"/>
 			<div class="main gunung-main">
@@ -20,9 +24,5 @@
 				<img src="./img/web/gunungtumpal.png" alt="about"/>
 			</div>
         </div>
-		
-		<!-- JS -->
-		<script src="./js/bootstrap.min.js"></script>
-		<script src="./js/jquery-3.1.1.js"></script>
     </body>
 </html>
