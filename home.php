55c4cf99e7062643bbcda3f6f9c711823e354443
Mahbub Haq Al Farisi <mahbub.alfarisi@gmail.com>
2018-05-19 21:31:45 +0700

    Perbaikan link home

---
+++ home.php
@@ -0,0 +1,144 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+		<title>Home - KBBI</title>
+    	<script src="./js/search.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+<script src="./js/jquery.min.js" charset="utf-8"></script>
+<script src="./js/renderedfont-0.8.min.js" charset="utf-8"></script>
+<script src="./js/jquery-3.1.1.js"></script>
+<script src="./js/carousel.js"></script>
+	
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<!-- Menyesuaikan layar -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Gambar tab browser -->
+<link rel='shortcut icon' href="./img/favicon.ico" type='image/x-icon'/>
+
+<!-- Bootstrap CSS -->
+<link href="./css/bootstrap.min.css" rel="stylesheet" >
+<link href="./css/bootstrap-theme.css" rel="stylesheet">
+ 
+<!-- Style CSS -->
+<link href="./css/style.css" rel="stylesheet">
+    </head>
+    <body>
+	<div class="container header-con navbar-fixed-top">
+	<nav class="navbar">
+		<div class="row">
+			<div class="col-xs-3 col-sm-2">
+				<div class="header-logo">
+					<a href="./home">
+						<img src="./img/logo/logoteks.png" class="img-responsive" alt="logo-kbbi" />
+					</a>
+				</div>
+			</div>
+			<div class="col-xs-9 col-sm-6">
+				<div class="menu-header">
+					<div class="collapse navbar-collapse" id="myNavbar">
+						<ul class="nav navbar-nav">
+							<li><a href="./home">BERANDA</a></li>
+							<li role="presentation" class="dropdown">
+								<a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+									TENTANG KAMI <span class="caret"></span>
+								</a>
+									<div class="dropdown-content">
+									<a href="./about">Tentang KBBI</a>
+									<a href="./sejbatikbetawi">Sejarah Batik Betawi</a>
+									<a href="./motifbatikbetawi">Motif Batik Betawi</a>
+									<a href="./kiat">Kiat Berkain Cantik</a>
+									<a href="./contact">Kontak Kami</a>
+								</div>
+							</li>
+							<li role="presentation" class="dropdown">
+								<a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+									GALERI <span class="caret"></span>
+								</a>
+								<div class="dropdown-content">
+									<a href="./batikmonas">Motif Batik Monas</a>
+									<a href="./batikonon">Motif Batik Ondel-Ondel</a>
+									<a href="./batiktumpal">Motif Tumpal Betawi</a>
+									<a href="./gunungtumpal">Motif Gunung Tumpal</a>
+									<a href="./ononpucuk">Motif Ondel-Ondel Pucuk Rebung</a>
+								</div>
+							</li>
+							<li><a href="./event">EVENT</a></li>
+							<li><a href="./member">MEMBER KAMI</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-3 col-sm-1">
+				<div class="social-header">
+					<a href="#" class="btn-social">
+						<img src="./img/icon/twitlogo.png" class="img-rounded" alt="Twitter Button" />
+					</a>
+					<a href="#" class="btn-social">
+						<img src="./img/icon/fblogo.png" class="img-rounded" alt="Facebook Button"/>
+					</a>
+				</div>
+			</div>
+			<div class="col-xs-3 col-sm-3">
+				<div class="search-header">
+					<form method="get" action="http://www.google.com" target="blank" role="search">
+						<div class="input-group stylish-input-group">
+							<input type="text" class="form-control"  placeholder="Search" >
+								<span class="input-group-addon">
+									<button type="submit">
+										<span class="glyphicon glyphicon-search"></span>
+									</button>  
+								</span>
+							</input>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</nav>
+</div>
+        <div class="container kbbi-con home-con">
+			<div id="carousel" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#carousel" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel" data-slide-to="1"></li>
+					<li data-target="#carousel" data-slide-to="2"></li>
+				</ol>
+
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner" role="listbox">
+					<div class="item active">
+						<img src="./img/web/homepage1.png" alt="gambar1" height="100%">
+					</div>
+					<div class="item">
+						<img src="./img/web/homepage2.png" alt="gambar2">
+						<div class="carousel-button-rm">
+							<button type="button" class="btn">
+								<a href="./kiat">Read More</a>
+							</button>
+						</div>
+					</div>
+					<div class="item">
+						<img src="./img/web/homepage3.png" alt="gambar3">
+						<div class="carousel-button-rm">
+							<button type="button" class="btn">
+								<a href="./motifbatikbetawi">Read More</a>
+							</button>
+						</div>
+					</div>
+				</div>
+
+				<!-- Controls -->
+				<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+					<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+					<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>     
+        </div>
+    </body>
+</html>
